5ce950c0c39e45332dd1694718fff928dd0413cf
saken <saken.bolatzhanuly@gmail.com>
2020-08-18 16:01:10 +0600

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      dark
+      color="primary"
+    >
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+      color="primary"
+      dark
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      fixed: false,
+      title: 'BGS user profile'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+      </div>
+      <v-card class="elevation-12" max-width="700">
+        <v-toolbar color="primary" dark flat>
+          <v-toolbar-title>Войти</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-card-text>
+          <v-text-field
+            v-model="email"
+            label="Адрес электронной почты"
+            color="primary"
+          />
+          <v-text-field
+            v-model="password"
+            label="Пароль"
+            type="password"
+            color="primary"
+          />
+          <div style="font-size: 15px" class="mt-4">
+            У вас еще нет аккаунта?
+            <span
+              class="primary--text"
+              style="cursor: pointer"
+              @click="toRegistration"
+            >
+                    Регистрация
+                  </span>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn class="primary theme--dark" @click="login">
+            Войти
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  layout: 'default',
+  data: () => ({
+    email: '',
+    password: ''
+  }),
+  components: {
+  },
+  methods: {
+    login () {
+
+    },
+    toRegistration () {
+
+    }
+  }
+}
+</script>
